cf6e4cc49087787a3fb55b556ec026233bda2bc4
Barış Ekim <ekimb@mit.edu>
2020-08-19 14:25:48 +0300

    add Ns of length of insert

--- src/buckets.rs
+++ src/buckets.rs
@@ -108,9 +108,10 @@ pub fn query_buckets(int_to_minimizer: &mut HashMap<u64, String>, read_to_seq_po
         let mut insert = String::new();
         //println!("\n{:?}", (consensus[i], consensus[i+1]));
         if !pair_map.contains_key(&(consensus[i], consensus[i+1])) {
-            //let alts : Vec<String> = pair_map.clone().into_iter().filter(|(a, b)| a.0 == consensus[i]).map(|tuple| tuple.1).collect();
+            let alts : Vec<String> = pair_map.clone().into_iter().filter(|(a, b)| a.0 == consensus[i]).map(|tuple| tuple.1).collect();
             //println!("{}", alts.len());
-            insert = int_to_minimizer[&consensus[i]].to_string() + "N";
+            insert = int_to_minimizer[&consensus[i]].to_string();
+            for _ in 0..alts[0].len() {insert.push_str("N")};
         }
         else { insert = pair_map[&(consensus[i], consensus[i+1])].to_string();}
         consensus_str.push_str(&insert);
