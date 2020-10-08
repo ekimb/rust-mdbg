232f004cabc4551952e8ac1a78e29f75e4d98c1b
Barış Ekim <ekimb@mit.edu>
2020-10-08 15:19:04 +0300

    change poa threshold

--- src/read.rs
+++ src/read.rs
@@ -196,7 +196,7 @@ impl Read {
                 }
             }   
         }
-        let mut bucket_reads : Vec<(&Read, f64)> = bucket_reads.iter().map(|seq| (*seq, minimizers::dist(self, seq, &params))).filter(|(seq, dist)| *dist < 0.1).collect();
+        let mut bucket_reads : Vec<(&Read, f64)> = bucket_reads.iter().map(|seq| (*seq, minimizers::dist(self, seq, &params))).filter(|(seq, dist)| *dist < 0.05).collect();
         bucket_reads.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
         for (read, dist) in bucket_reads.iter() {
             let transformed = &read.transformed;
