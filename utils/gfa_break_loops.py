b82b7cace6fba530612b383ea0fd4be200bad127
rayan <rayan.chikhi@pasteur.fr>
2020-10-25 22:06:45 +0100

    patch to gfa_break_loops to also break self loops

--- utils/gfa_break_loops.py
+++ utils/gfa_break_loops.py
@@ -1,8 +1,14 @@
-# this script artificially breaks self-loop nodes by cutting one of the extremity edges
+# this script artificially breaks loop nodes by cutting one of the extremity edges
 # e.g. 
 # L x + y - 10M
 # L x + y + 10M
 # will remove one of the two lines
+# also removes 
+# L x + y + 10M
+# L x + y + 10M
+# (it doesn't care about orientation)
+
+# new: and also let's make it remove x<->x loops
 
 seen_edges = set()
 import sys, traceback
@@ -14,6 +20,8 @@ for line in open(sys.argv[1]):
         e_in, e_out = line.split()[1], line.split()[3]
         e_tuple = tuple(sorted([e_in, e_out]))
         to_remove = e_tuple in seen_edges
+        if e_in == e_out: # self loop
+            to_remove = True
         seen_edges.add(e_tuple)
         if not to_remove:
             print(line.strip())
